cba4f44752ac055c6cf22987dc43c8463e830266
João Victor <jao.victor.dp@gmail.com>
2022-02-16 17:41:04 -0300

    Replace db_password and db_name in connection

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 // phpinfo();
 
 try {
-    mysqli_connect("db", "root", "base_php_docker") or die(mysqli_error());
+    $conn = mysqli_connect("db", "root", "".getenv('MYSQL_ROOT_PWD', '')."", "".getenv('MYSQL_DB', '')."");
     echo "Connected to MySQL<br />";
+    var_dump($conn);
+    exit;
+
 } catch (\Exception $ex) {
     echo "Falha ao conectar";
     echo "<pre>";
